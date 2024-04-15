5bd2cddab73ff4da686a7125dbe8b257e6bad15a
Artem Galukhin <untimid@mail.ru>
2024-04-15 02:24:09 -0300

    install icons package

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const { nextui } = require('@nextui-org/react')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  content: ['./src/**/*.{html,js}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
